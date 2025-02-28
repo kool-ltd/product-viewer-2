abd6c5a1ae0c862071d29b2700cf12660173b416
kool-ltd <kooldesignteam@gmail.com>
2025-02-28 16:10:10 +0800

    Update InteractionManager.js

--- js/InteractionManager.js
+++ js/InteractionManager.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.0/build/three.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { DragControls } from 'three/addons/controls/DragControls.js';
 
